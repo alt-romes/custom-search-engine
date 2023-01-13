cd86129cbfdc36be930f58efdf24e4ff63c1702d
romes <rodrigo.m.mesquita@gmail.com>
2023-01-13 00:04:36 +0000

    tweaks

--- src/CustomSearchEngine.hs
+++ src/CustomSearchEngine.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 module CustomSearchEngine where
@@ -15,15 +16,16 @@ import qualified Data.Text.IO as T
 import Control.Exception (throwIO)
 import Control.Applicative
 import Control.Monad
---------------------------------------------
-import Text.HTML.Scalpel
 
 import Network.HTTP.Client (HttpException)
+import Text.HTML.Scalpel
 
 import Cob.RecordM.TH
 import Cob.RecordM.Query
 import Cob.Ref
 import Cob
+
+-----------------------------------------------------------------
 
 data IndexEntry = IndexEntry { _user    :: String
                              , _url     :: String
@@ -32,35 +34,48 @@ data IndexEntry = IndexEntry { _user    :: String
                              , _headings :: Maybe Text
                              , _content :: Maybe Text
                              }
-
 mkRecord ''IndexEntry "Index Entries" ["User", "URL", "Title", "Description", "Headings", "Content"]
+
+-----------------------------------------------------------------
 
 -- | Get links according to query
 searchFor :: String -> Cob [IndexEntry]
 searchFor q = map snd <$> search ((fromString q) { _size = 30 })
 
+-----------------------------------------------------------------
+
 -- | Adds a link to your index
 addLink :: URL -> Cob ()
 addLink url = (`catch` (\(e :: HttpException) -> liftCob $ print e)) $ do
+
   Just index <- liftCob $ scrapeURL url (scraper url "romes")
+
   _ <- add index
+
   liftCob $ putStrLn ("Added: " <> url)
 
   where
     scraper :: String -> String -> Scraper Text IndexEntry
     scraper url user = do
-      title <- Just <$> (text "title") <|> pure Nothing
+
+      title <- Just <$> text "title" <|> pure Nothing
+
       desc  <- Just <$> attr "content" ("meta" @: ["name" @= "description"]) <|> pure Nothing
+
       h1 <- texts "h1"
       h2 <- texts "h2"
       h3 <- texts "h3"
       h4 <- texts "h4"
+
       content <- Just . T.unlines <$> texts "p"
+
       let headings = case h1 <> h2 <> h3 <> h4 of
                        [] -> Nothing
                        hs -> Just $ T.unwords hs
+
       pure (IndexEntry user url title desc headings content)
 
+-----------------------------------------------------------------
 
 -- | Import from firefox exported bookmarks as html
 importFromFirefoxExport :: FilePath -> Cob ()
@@ -73,26 +88,24 @@ importFromFirefoxExport file = do
     Nothing -> liftCob (fail "Couldn't parse file")
 
     Just links -> do
-      -- todo: Don't ignore titles and tags...
 
       mapConcurrently (addLink . T.unpack . fst) links
       pure ()
 
   where
+
     scrpr :: Scraper Text [(Text, Text)] -- ^ (URL, Title)
     -- scrape bookmark folders (triply nested dl)
-    scrpr = chroot ("dl" // "dl" // "dl") $ do
+
+    scrpr = chroot ("dl" // "dl" // "dl") do
+
       hrefs  <- attrs "href" "a"
+
       titles <- texts "a"
-      pure (zip hrefs titles)
 
-  -- way too exagerated for this
-    -- File.toBytes file
-  -- & U.decodeUtf8
-  -- & U.lines Fold.toList   -- Serial String (line)
-  -- & S.mapM  (putStrLn)
-  -- & S.drain
+      return (zip hrefs titles)
 
+-----------------------------------------------------------------
 
 instance Show IndexEntry where
   show (IndexEntry _ u t d _ _) = (<> u <> "\n\n") $ T.unpack $
